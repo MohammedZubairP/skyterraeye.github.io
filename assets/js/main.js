242730b2b88d6a21193ae21d92fcaa208ef71fdc
MohammedZubairP <162498660+MohammedZubairP@users.noreply.github.com>
2026-02-20 14:43:00 +0400

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
-document.querySelectorAll('[data-year]').forEach(el=>el.textContent=new Date().getFullYear());
+(() => {
+  const burger = document.querySelector('[data-burger]');
+  const menu = document.querySelector('.menu');
+  if (burger && menu){
+    burger.addEventListener('click', () => menu.classList.toggle('open'));
+  }
+  const y = document.querySelector('[data-year]');
+  if (y) y.textContent = new Date().getFullYear();
+})();
